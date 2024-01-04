bc613d505c4b57bbed4641bf3e9dff3a8d2cf6a1
Jordan Fox <jordan.fox@canstar.com.au>
2024-01-04 13:36:00 +1000

    change turbo replace to turbo update

--- app/controllers/concerns/render_exercise_instance_form_fields.rb
+++ app/controllers/concerns/render_exercise_instance_form_fields.rb
@@ -7,25 +7,25 @@ module RenderExerciseInstanceFormFields
   
       def render_exercise_instance_form_fields
         if @exercise_instance.exercise.category.name == "Cardio"
-          render turbo_stream: turbo_stream.replace(
+          render turbo_stream: turbo_stream.update(
             "exercise_form_fields",
             partial: "exercise_instances/cardio_form_fields",
             locals: { exercise_instance: @exercise_instance }
           )
         elsif @exercise_instance.exercise.category.name == "Yoga"
-          render turbo_stream: turbo_stream.replace(
+          render turbo_stream: turbo_stream.update(
             "exercise_form_fields",
             partial: "exercise_instances/yoga_form_fields",
             locals: { exercise_instance: @exercise_instance }
           )
         elsif @exercise_instance.exercise.category.name == "Other"
-          render turbo_stream: turbo_stream.replace(
+          render turbo_stream: turbo_stream.update(
             "exercise_form_fields",
             partial: "exercise_instances/other_form_fields",
             locals: { exercise_instance: @exercise_instance }
           )
         else
-          render turbo_stream: turbo_stream.replace(
+          render turbo_stream: turbo_stream.update(
             "exercise_form_fields",
             partial: "exercise_instances/strength_form_fields",
             locals: { exercise_instance: @exercise_instance }

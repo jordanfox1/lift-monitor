d48ff380966bd51e7f76cb9b4fb822a73b909d4e
jordanfox1 <jordanfox9415@gmail.com>
2024-03-31 23:06:11 +1000

    working on timer

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -3,20 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["timer", "display"]
   connect() {
-    // Get initial time from data attribute (optional)
-    this.targetTime = parseInt(this.element.dataset.targetTime) || 0; // Default to 0 seconds
+    this.initTimer();
+  }
+  
+  initTimer() {
+    this.targetTime = parseInt(this.element.dataset.targetTime);
     this.startTime = Date.now();
     this.updateTimer();
-    this._intervalId = setInterval(this.updateTimer.bind(this), 1000); // Update every second
+    this._intervalId = setInterval(this.updateTimer.bind(this), 1000); // run update timer every second
   }
 
   disconnect() {
-    clearInterval(this._intervalId); // Stop timer on disconnect
+    clearInterval(this._intervalId); 
   }
 
   updateTimer() {
-    const elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
-    const remainingTime = Math.max(0, this.targetTime - elapsedTime);
+    if (this.currentDisplayTime === 0) {
+      clearInterval(this._intervalId);
+      return;
+    }
+
+    this.elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
+    const remainingTime = Math.max(0, this.targetTime - this.elapsedTime);
+    this.currentDisplayTime = 60 - this.elapsedTime;
 
     // Update display element with formatted time (minutes:seconds)
     this.displayTarget.textContent = `${Math.floor(remainingTime / 60)}:${remainingTime % 60}`;
@@ -24,11 +33,24 @@ export default class extends Controller {
 
   incrementTimerBy20() {
     this.targetTime = Math.max(0, this.targetTime + 20);
+    this.startTime = Date.now();
     this.updateTimer();
   }
   
   decrementTimerBy20() {
     this.targetTime = Math.max(0, this.targetTime - 20);
+    this.startTime = Date.now();
     this.updateTimer();
+  }
+
+  startPause() {
+    if (this.startTime) {
+      clearInterval(this._intervalId);
+      this.startTime = null;
+      return;
+    }
+
+    this._intervalId = setInterval(this.updateTimer.bind(this), 1000);
+    this.startTime = Date.now() - this.elapsedTime * 1000;
   }
 }

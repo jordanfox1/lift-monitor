eab2544069686e6013e2ce3d8027c57c0392ffbf
jordanfox1 <jordanfox9415@gmail.com>
2024-04-01 14:11:30 +1000

    fix

--- app/models/exercise_instance.rb
+++ app/models/exercise_instance.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ExerciseInstance < ApplicationRecord
-  enum :status, { warmup: 0, easy: 1, moderate: 2, hard: 3, technical_failure: 4, absolute_failure: 5 }
+  enum :intensity, { warmup: 0, easy: 1, moderate: 2, hard: 3, technical_failure: 4, absolute_failure: 5 }
   belongs_to :exercise_log
   belongs_to :exercise
   has_many :set_instances, dependent: :destroy

b00ebebbd2e834cf191325eec2944857ccfa3278
jordanfox1 <jordanfox9415@gmail.com>
2024-04-01 14:00:40 +1000

    update exercise_instance model validation

--- app/models/exercise_instance.rb
+++ app/models/exercise_instance.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class ExerciseInstance < ApplicationRecord
+  enum :status, { warmup: 0, easy: 1, moderate: 2, hard: 3, technical_failure: 4, absolute_failure: 5 }
   belongs_to :exercise_log
   belongs_to :exercise
   has_many :set_instances, dependent: :destroy
 
   validates :start_time, presence: true
+  validates :set_number, presence: true
 end
